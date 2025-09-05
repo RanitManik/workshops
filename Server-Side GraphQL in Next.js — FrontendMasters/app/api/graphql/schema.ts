c1491666a36b83c74f5d373cedc2dc136c360a83
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2025-09-05 16:56:03 +0530

    updated server-side graphql course

--- Server-Side GraphQL in Next.js — FrontendMasters/app/api/graphql/schema.ts
+++ Server-Side GraphQL in Next.js — FrontendMasters/app/api/graphql/schema.ts
@@ -1,5 +1,50 @@
 const schema = `#graphql
 
+    union SearchResult = Tweet | Profile
+
+    type User {
+        id: ID!
+        username: String!
+        email: String!
+    }
+
+    type Tweet {
+        id: ID!
+        text: String!
+        author: User!
+    }
+
+    type Profile {
+        id: ID!
+        bio: String
+        avatarUrl: String
+    }
+
+    interface Character {
+        name: String!
+        age: Int!
+        height: Float!
+    }
+
+    type Person implements Character {
+        name: String!
+        age: Int!
+        height: Float!
+        backgroundStory: String!
+    }
+
+    type Droid implements Character {
+        name: String!
+        age: Int!
+        height: Float!
+        primaryFunction: String!
+    }
+
+    type Query {
+        me: Person!
+        characters: [Character!]! # Non-nullable list of non-nullable Character
+        search: [SearchResult!]!
+    }
 `;
 
 export default schema;
